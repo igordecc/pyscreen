d0c263803ffa1408fb4b4daa3d4b88dfc9c9dff3
Игорь Десятов <DesyatovIV@gkosar.corp>
2020-11-06 16:27:55 +0400

    Complete tests for coil text

--- v1.0/HeaderedImage.py
+++ v1.0/HeaderedImage.py
@@ -13,21 +13,33 @@ class HeaderedImage:
         self.url = url
         self.data = data
         self.savepath = savepath
+        font = ImageFont.load_default().font
+        self.fontd = {"padding": 10,
+                 "font": font,
+                 "font_size": None,
+                 "char_w": (font.getsize("1"))[0],
+                 "char_h": (font.getsize("1"))[1]
+                 }
 
-    def coilTheText(self, text, width):
+    def coilTheText(self, text, width: int, fontd):
+        """
+
+        :param text: string, or list of strings
+        :param width: pixel width of text line
+        :param fontd: special dictionary, predefined in __init__ of the class
+        :return: coiled text
+        """
+        padding = fontd["padding"]
+        font = fontd["font"]
+        char_w = fontd["char_w"]
         # coil list
 
         def coilText(text):
-            # hardcoded
-            padding = 10
-            font = ImageFont.load_default().font
-            font_size = None
-
             # not hardcoded
-            char_w, char_h = font.getsize("1")
             find_number_of_chars = lambda w, p, c: (w - 2 * p) // c
-            number_of_chars = find_number_of_chars(width, padding, char_w)
-
+            number_of_chars = round(find_number_of_chars(width, padding, char_w))
+            if number_of_chars < 1:
+                number_of_chars = 1
             # text wrap consume width in characters
             # that means prog mast calculate width in number of characters beforehand
             coiledText = textwrap.wrap(text, width=(number_of_chars))
@@ -42,29 +54,45 @@ class HeaderedImage:
         else:
             return coilText(text)
 
+    def testCoilTheText(self):
+        smallest_case = self.coilTheText("OneONeOneoneOneOne", 20, self.fontd)
+        print(smallest_case)
+        largest_case = self.coilTheText("OneONeOneoneOneOne", 2000, self.fontd)
+        print(largest_case)
+        fractional_case = self.coilTheText("OneONeOneoneOneOne", 73.5, self.fontd)
+        print(fractional_case)
+        negative_case = self.coilTheText("OneONeOneoneOneOne", -73.5, self.fontd)
+        print(negative_case)
+
 
     def createHeaderImage(self, url, time, img_w):
         """create Header image based on img_w, from list of lines"""
 
         # determine hight of header
-        padding = 10
-        font = ImageFont.load_default().font
-        char_w, char_h = font.getsize("1")
-        lines = textwrap.wrap(url, width=((img_w - 2 * padding) // char_w))
-        line_w, line_h = font.getsize(lines[0])
+        padding = self.fontd["padding"]
+        char_h = self.fontd["char_h"]
+
+
 
         # Create new image
-        calcHeaderH = lambda h, p, l: (h + p)*(l + 1) + p   # header height = row height * (row number + 1 data row)
-        header_H = calcHeaderH(line_h, padding, len(lines))
-        headerImg = Image.new('RGB', (img_w, header_H), color=(255, 255, 255))
-        draw = ImageDraw.Draw(headerImg)
+        text = [url, time]
+        lines = self.coilTheText(text, img_w, self.fontd)
 
-        # Place text on new image
-        y = padding
-        for line in lines:
-            draw.multiline_text((padding, y), line, fill=(0, 0, 0))
-            y += line_h + padding
-        draw.text((padding, y + line_h), str(time), fill=(0, 0, 0))
+        calcHeaderH = lambda h, p, l: (h + p)*l + p   # header height = row height * row number
+        header_H = calcHeaderH(char_h, padding, len(lines))
+
+
+        def linesOnImage(header_w, header_h):
+            headerImg = Image.new('RGB', (header_w, header_H), color=(255, 255, 255))
+            draw = ImageDraw.Draw(headerImg)
+
+            # Place text on new image
+            y = padding
+            for line in lines:
+                draw.multiline_text((padding, y), line, fill=(0, 0, 0))
+                y += char_h + padding
+
+            return headerImg
 
         return headerImg
 
@@ -90,3 +118,14 @@ class HeaderedImage:
         multiImage.save(savepath)
         return savepath
 
+    def test(self):
+        self.testCoilTheText()
+
+
+if __name__ == '__main__':
+    path = "D:\\test.png"
+    url = "https://stackoverflow.com/questions/16373425/add-text-on-image-using-pil"
+    data = "30.10.2020"
+    savepath = 'test.png'
+    himg = HeaderedImage(path, url, data, savepath)
+    himg.test()
